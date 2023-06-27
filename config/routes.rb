5f063ed812bfa65796806fd7046ce041269095c1
marcio <pnsantos25@gmail.com>
2023-06-28 00:31:26 +0100

    inicializando o repositorio para estudar rails

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get 'contato/index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'home#index'
+  get "sobre", to: "home#sobre"
+  get "contato", to: "contato#index"
+  post "contato/cadastrar", to: "contato#cadastrar"
+end
